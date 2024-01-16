d48db6d5c93528d27c1ca57112c6ace36fc40fcd
patrimj <patriimj1@gmail.com>
2024-01-16 12:23:55 +0100

    borrado

--- 7. NODE/Migrations/routes/commentsRoutes.js
+++
@@ -1,9 +0,0 @@
-const {Router } = require('express');
-const controlador = require('../controllers/commentController');
-const router = Router();
-
-router.get('/', controlador.getComments);
-router.get('/:id?', controlador.getCommentsId);
-
-
-module.exports = router;
